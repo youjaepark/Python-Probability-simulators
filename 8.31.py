1bf052db52cc3ced4cf0a9e379c3d18338e345f7
youjaepark <qkrdbwo0506@gmail.com>
2023-04-19 12:17:56 +0900

    8.31

--- 8.31.py
+++ 8.31.py
@@ -3,12 +3,13 @@ print('저는 한국나이 18살로, 헤이븐 기독학교에 재학중입니�
 print('저의 가족은 부모님, 형과 여동생 그리고 반려견이 있습니다.')
 print('전 졸립니다.')
 
-StudentName='남기헌'
-Korean Age=18
+StudentName = '남기헌'
+Korean Age = 18
 ='용인시 수지구'
-hobby='게임'
+hobby = '게임'
 
-print('안녕하세요 제 이름은',name,'입니다.')
-print('제 나이는',age,'살입니다.')
-print('저는',house,'에 거주합니다.')
-print('제 취미는',hobby,'입니다.')
+print('안녕하세요 제 이름은', name, '입니다.')
+print('제 나이는', age, '살입니다.')
+print('저는', house, '에 거주합니다.')
+print('제 취미는', hobby, '입니다.')
+print("안녕")
